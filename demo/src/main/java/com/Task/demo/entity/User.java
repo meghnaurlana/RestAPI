4f54af7baf2be927beced66e62bb37b674a8e470
meghnaurlana <gunameghana.u@imaginnovate.com>
2023-06-14 22:13:48 +0530

    Added entity class

--- demo/src/main/java/com/Task/demo/entity/User.java
+++ demo/src/main/java/com/Task/demo/entity/User.java
@@ -5,17 +5,25 @@ import lombok.Data;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @Data
 public class User {
-    @Id
-    @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int userId;
-    @Column(name = "USER_NAME")
-    private String userName;
-    @Column(name = "USER_ADDRESS")
-    private String userAddress;
 
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "salary")
+    private float salary;
+
+    public User() {
+    }
+
+    public User(int id, String name, float salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
 }
